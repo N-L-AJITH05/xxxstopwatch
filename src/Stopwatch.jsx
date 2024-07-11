18dfc6ce616816acb2d9cbce23ea3804b502aa63
N-L-AJITH05 <ajithram3999@gmail.com>
2024-07-11 13:47:59 +0530

    second update

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -29,7 +29,7 @@ function Stopwatch() {
   return (
     <div>
       <h1>Stopwatch</h1>
-      <h1>Time : {formatTime(time)}</h1>
+      <h1>Time: {formatTime(time)}</h1>
       <button onClick={handleStartStop}>{isRunning ? "Stop" : "Start"}</button>
       <button onClick={handleReset}>Reset</button>
     </div>
